631388bb6a5d186634f3072f5e96028628587c62
rewna20 <anwer.benabdallah01@gmail.com>
2026-02-12 01:20:34 +0100

    Updated pomodoro feature

---
+++ Pomodoro/pomodoro.js
@@ -0,0 +1,6 @@
+const pEl=document.getElementById("startParagraph")
+const startBtn=document.getElementById("start")
+
+startBtn.addEventListener("click",function(){
+    pEl.style.display="block";
+})
