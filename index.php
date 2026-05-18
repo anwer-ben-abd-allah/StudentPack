5c0637480a5db4fda565e6fd8aab751218aec1a3
rewna20 <anwer.benabdallah01@gmail.com>
2026-05-18 10:40:27 +0100

    implemented PHP & improved pomodoro functionality
    added navbar.php instead of writing the navbar in each file. Pomodoro timer now running ( saving sessions soon )

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Pack</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Clash+Display:wght@400;600;700&family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  
+  <link rel="stylesheet" href="./Time Table/timetable.css">
+</head>
+<body>
+  
+<?php
+// including the navbar 
+include 'navbar.php';
+?>
+
+
+
+
+
+<!-- jQuery + Bootstrap JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<!-- Global auth script: shows username in navbar on every page -->
+<script src="navbar_auth.js"></script>
+</body>
+</html>
